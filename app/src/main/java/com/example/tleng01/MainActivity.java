a968062aca0b7de52fb1c7813e4799f5b4ba5b88
CESAR ALGUERA <cesar.alguera@cadelga.com>
2023-02-25 23:34:31 -0600

    Initial commit

---
+++ app/src/main/java/com/example/tleng01/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.tleng01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText txtNombre, txtApellido;
+    Button btnAgregar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtNombre = (EditText) findViewById(R.id.txtNombre);
+        txtApellido = (EditText) findViewById(R.id.txtApellido);
+        btnAgregar = (Button) findViewById(R.id.btnAgregar);
+
+        btnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, txtNombre.getText()+" "+txtApellido.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
